14917f282b6be3aadbb5f3242214f0591cd2dd6d
Abhinandan Thakur <distractionFree.C.C.Parent@gmail.com>
2025-09-06 10:11:38 +0530

    some bugs fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas, CustomCursor } from "./components";
 
 const App = () => {
   return (
     <BrowserRouter>
+    <CustomCursor />
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
