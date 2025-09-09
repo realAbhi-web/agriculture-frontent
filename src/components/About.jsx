be480e1d6003dba1793e451581e964df4a291747
Abhinandan Thakur <distractionFree.C.C.Parent@gmail.com>
2025-09-09 16:56:10 +0530

    done some changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,33 +8,36 @@ import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 import AnimatedText from "./AnimatedText";
 
-const ServiceCard = ({ index, title, icon }) => (
+const ServiceCard = ({ index, title, icon, description }) => (
   <Tilt className='xs:w-[250px] w-full'>
     <motion.div
-      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      variants={fadeIn("right", "spring", index * 1.3, 1.55)}
       className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
     >
       <div
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
-        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+        className='group bg-tertiary rounded-[20px] py-5 px-6 min-h-[280px] flex justify-center items-center flex-col text-center transition-all duration-300'
       >
         <img
           src={icon}
-          alt='web-development'
-          className='w-16 h-16 object-contain'
+          alt={title}
+          className='w-16 h-16 object-contain mb-4'
         />
 
-        <h3 className='text-white text-[20px] font-bold text-center'>
+        {/* Show title by default */}
+        <h3 className='text-white text-[20px] font-bold group-hover:hidden'>
           {title}
         </h3>
+
+        {/* Show description on hover */}
+<p className='opacity-0 group-hover:opacity-100 max-h-0 group-hover:max-h-40 overflow-hidden transition-all duration-500 text-white text-[15px] leading-[22px] mt-2'>
+  {description}
+</p>
+
       </div>
     </motion.div>
   </Tilt>
 );
+
 
 const About = () => {
   return (
